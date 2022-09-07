bfb050ab09b85e71353b061b23e08f138f07f5d9
bvtucker <59457738+bvtucker@users.noreply.github.com>
2022-09-07 10:02:23 -0600

    Estonian addition

--- sample.py
+++ sample.py
@@ -1,6 +1,8 @@
 from wuggy import WuggyGenerator
 
+from my_custom_plugin.orthographic_estonian import LanguagePlugin
+
 g = WuggyGenerator()
-g.load("orthographic_english")
-for w in g.generate_classic(["trumpet"]):
+g.load("orthographic_estonian", LanguagePlugin())
+for w in g.generate_classic(["jõgi"]):
     print(w)
