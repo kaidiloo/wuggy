248d004285baebbcc5fbb214f280f2d8c5464f70
bvtucker <59457738+bvtucker@users.noreply.github.com>
2022-09-09 07:09:53 -0600

    update

--- EstonianGenerator.py
+++ EstonianGenerator.py
@@ -16,7 +16,7 @@ g = WuggyGenerator()
 g.load("orthographic_estonian", OfficialLanguagePlugin())
 
 lines = []
-with open('est_wordlist.txt', "r") as text_file:
+with open('LexEst_stiimulid.tsv', "r") as text_file:
 # with open('est_List.txt', "r") as text_file:
     lines = text_file.readlines()
     for i,s in enumerate(lines):
@@ -24,7 +24,7 @@ with open('est_wordlist.txt', "r") as text_file:
 
 # pseudoword_matches = g.generate_classic(["jõgi","aare"])
 pseudoword_matches = g.generate_classic(lines)
-g.export_classic_pseudoword_matches_to_csv(pseudoword_matches, "./pseudowords.csv")
+g.export_classic_pseudoword_matches_to_csv(pseudoword_matches, "./lextale_pseudowords.csv")
 # for w in g.generate_classic(["jõgi"]):
 #     print(w["pseudoword"])
 #     # print(w)
